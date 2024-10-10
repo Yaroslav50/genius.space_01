71110823c46507ba5ee9ab63f9d76518df2273a0
Yaroslav50 <z7664008@gmail.com>
2024-10-10 14:59:36 +0300

    formats-section updated with swiper.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,8 +1,15 @@
 const mobileMenu = document.querySelector('.mobile-menu');
+const bodyLock = document.querySelector('.body');
+
+
 const menuBtnOpen = document.querySelector('.menu-btn-open');
 const menuBtnClose = document.querySelector('.menu-btn-close');
 
+
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
+const toggleLock = () => bodyLock.classList.toggle('lock');
 
 menuBtnOpen.addEventListener('click', toggleMenu);
+menuBtnOpen.addEventListener('click', toggleLock);
 menuBtnClose.addEventListener('click', toggleMenu);
+menuBtnClose.addEventListener('click', toggleLock);
