203e5185716ecd44dc712e1fdb9c499b7e9144dd
Yaroslav50 <z7664008@gmail.com>
2024-10-11 15:57:50 +0300

    footer updated with media querries

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,13 @@
-const mobileMenu = document.querySelector('.mobile-menu');
-const bodyLock = document.querySelector('.body');
+const mobileMenu = document.querySelector(".mobile-menu");
+const bodyElem = document.body;
 
+const menuBtnOpen = document.querySelector(".menu-btn-open");
+const menuBtnClose = document.querySelector(".menu-btn-close");
 
-const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const toggleMenu = () => mobileMenu.classList.toggle("is-open");
+const toggleLock = () => bodyElem.classList.toggle("lock");
 
-
-const toggleMenu = () => mobileMenu.classList.toggle('is-open');
-const toggleLock = () => bodyLock.classList.toggle('lock');
-
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnOpen.addEventListener('click', toggleLock);
-menuBtnClose.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleLock);
+menuBtnOpen.addEventListener("click", toggleMenu);
+menuBtnOpen.addEventListener("click", toggleLock);
+menuBtnClose.addEventListener("click", toggleMenu);
+menuBtnClose.addEventListener("click", toggleLock);
